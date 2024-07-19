06525da0fc5a2e05d98184f01c006753d8df06aa
Fachri Naufal <fachrinfl@gmail.com>
2024-07-19 11:21:39 +0700

    add menubar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Menubar from "./components/Menubar"
 import Navbar from "./components/Navbar"
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Menubar />
     </>
   )
 }
